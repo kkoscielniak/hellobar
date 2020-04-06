399e903020c53f467d9fff4394d575c3d0796d59
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2020-04-06 16:44:13 +0200

    Initial commit (based on nibar)

---
+++ lib/styles.jsx
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    fg: "rgba(255,255,255,0.75)",
+    dim: "rgba(255,255,255,0.5)",
+    bg: "#1c1c1c",
+    red: "#ff8700",
+    accent: "#5fafaf"
+  },
+  fontSize: "11px",
+  lineHeight: "24px",
+  fontWeight: 500,
+  fontFamily: "'SF Mono', monospace"
+}
